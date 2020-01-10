2785ad7ec741df08a00681e279ed566acef12554
Laxmikant <=>
2020-01-10 14:05:26 +0530

    Updates

---
+++ precache-manifest.031890d884a6a54b93c929dfc06b966b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf1e086408bf0b33f67a076fb38ddff",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "b2d7c338951f44dd308a",
+    "url": "/monster/static/css/main.640ec0b4.chunk.css"
+  },
+  {
+    "revision": "a377cc79a59acb42b90e",
+    "url": "/monster/static/js/2.f8ebc5f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster/static/js/2.f8ebc5f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2d7c338951f44dd308a",
+    "url": "/monster/static/js/main.e297d87b.chunk.js"
+  },
+  {
+    "revision": "a1650fff6b095063f44e",
+    "url": "/monster/static/js/runtime-main.cbcc7347.js"
+  }
+]);
